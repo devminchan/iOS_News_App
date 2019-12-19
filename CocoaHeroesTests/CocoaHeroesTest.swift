65455e0527a218bdc1e0d160ec3f7c0e40614fff
minchanKim <minchan.dev@gmail.com>
2019-12-19 14:31:42 +0900

    [Edit] 프로젝트 이름 변경 PodPod -> CocoaHeroes

--- CocoaHeroesTests/CocoaHeroesTest.swift
+++ CocoaHeroesTests/CocoaHeroesTest.swift
@@ -9,7 +9,7 @@
 import XCTest
 @testable import PodPod
 
-class PodPodTests: XCTestCase {
+class CocoaHeroesTest: XCTestCase {
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
--- CocoaHeroesUITests/CocoaHeroesUITest.swift
+++ CocoaHeroesUITests/CocoaHeroesUITest.swift
@@ -8,7 +8,7 @@
 
 import XCTest
 
-class PodPodUITests: XCTestCase {
+class CocoaHeroesUITest: XCTestCase {
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
